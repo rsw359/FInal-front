b90170541440c085a4732253356fd1b2596fb672
Roger Wells <rsw359@gmail.com>
2022-04-01 08:23:03 +0900

    init

---
+++ src/components/Items.js
@@ -0,0 +1,37 @@
+import { Component } from 'react';
+import { Accordion } from 'react-bootstrap';
+
+class Items extends Component {
+
+  render() {
+    console.log('items list', this.props.itemsList);
+    return (
+      <section>
+        <h2>Items...</h2>
+        <Accordion defaultActiveKey="0">
+          
+          {
+            this.props.itemsList.map((item, idx) =>
+            
+              <Accordion.Item eventKey={idx} key={item._id}>
+                <Accordion.Header>{item.name}</Accordion.Header>
+                <Accordion.Body>
+                  {item.description}
+                </Accordion.Body>
+              </Accordion.Item>
+              
+            )
+          }
+        </Accordion>
+
+      
+
+
+      </section>
+    );
+  }
+}
+
+
+
+export default Items;
